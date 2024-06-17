e31213da1ca0eb08ff66c3504c9101add8f72fc9
Jurgen Kluft2 <jurgen@virtuosgames.com>
2024-06-17 11:12:23 +0800

    * fix merge conflict

--- source/main/include/ccompress/c_compress.h
+++ source/main/include/ccompress/c_compress.h
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:source/main/include/ccompress/ccompress.h
 // c_compress.h - Core Compression API - 
 #ifndef __CCOMPRESS_COMPRESSOR_INTERFACE_H__
 #define __CCOMPRESS_COMPRESSOR_INTERFACE_H__
@@ -7,25 +6,6 @@
 #pragma once
 #endif
 
-
-namespace ncore
-{
-	class xcompressor
-	{
-	public:
-		virtual void		reset() = 0;
-		virtual void		compress(u8 const* src, u8 const* src_end, u8 * dst, u8 *& dst_end, u8 * dst_eos) = 0;
-		virtual void		decompress(u8 const* src, u8 const* src_end, u8 * dst, u8 *& dst_end, u8 * dst_eos) = 0;
-	};
-}
-
-========
-#ifndef __CCOMPRESS_COMPRESSOR_INTERFACE_H__
-#define __CCOMPRESS_COMPRESSOR_INTERFACE_H__
-#include "ccore/c_target.h"
-#ifdef USE_PRAGMA_ONCE
-#pragma once
-#endif
 
 namespace ncore
 {
@@ -38,5 +18,4 @@ namespace ncore
 	};
 }
 
->>>>>>>> bbf5013c48a58aebb21060c016a18431215953b8:source/main/include/ccompress/c_compress.h
 #endif	///< __CCOMPRESS_COMPRESSOR_INTERFACE_H__
